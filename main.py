ed8291372b4a87ea6d8b6282e3a53843034caf87
TrushaAsarhad <zeolita123@gmail.com>
2024-04-26 12:45:31 +0530

    wew

--- main.py
+++ main.py
@@ -45,9 +45,70 @@ if selected_file:
         x_axis = st.selectbox("Select the X-axis",options= columns + ["None"],index=None)    
         y_axis = st.selectbox("Select the Y-axis",options= columns + ["None"], index=None)
 
-        plot_list =["Line Plot"," Bar Chart", "Scatter Plot","Distribution Plot"]
+        plot_list =["Line Plot","Bar Chart", "Scatter Plot","Distribution Plot"]
 
         selected_plot= st.selectbox("Select a Plot",options=plot_list,index=None)
+
+
+        st.write(x_axis)
+        st.write(y_axis)
+        st.write(selected_plot)
+
+
+    if st.button("Generate Plot"):
+        fig, ax =plt.subplots(figsize=(6,4))
+
+        if selected_plot =="Line Plot":
+            sns.lineplot(x=df[x_axis],y=df[y_axis], ax=ax)    
+
+
+        elif selected_plot =="Bar Chart":
+            sns.barplot(x=df[x_axis],y=df[y_axis], ax=ax) 
+
+
+        elif selected_plot =="Scatter Plot":
+            sns.scatterplot(x=df[x_axis],y=df[y_axis], ax=ax)
+
+        elif selected_plot =="Distribution Plot":
+            sns.histplot(x=df[x_axis],kde=True, ax=ax)    
+
+
+        ax.tick_params(axis="x", labelsize=10)
+        ax.tick_params(axis="y",labelsize=10)
+
+        plt.title(f"{selected_plot} of {y_axis} vs {x_axis}",fontsize=12)
+        plt.xlabel(x_axis,fontsize=10)
+        plt.ylabel(y_axis,fontsize=10)
+
+        st.pyplot(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
